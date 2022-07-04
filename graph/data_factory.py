c0b192f7c0209ba392cc16d8e47ea4d6cee23e5c
Tom <tom.iten@surfmachine.ch>
2022-07-04 16:55:30 +0200

    work

--- graph/data_factory.py
+++ graph/data_factory.py
@@ -46,12 +46,16 @@ class Employee:
         self.ln = Employee.LASTNAME_PREFIX + str(i) if add_ln else None
         # embed type node between employee and child nodes
         self.add_type_nodes = add_type_nodes
-        # otional weitgts for the edges
+        # optional weitgts for the edges
         self.weighted = weighted
         self.weight_id = weight_id
         self.weight_fn = weight_fn
         self.weight_ln = weight_ln
         self.weight_type_nodes = weight_type_nodes
+        # type nodes
+        self.id_type_node = Employee.ID_PREFIX + Employee.TYPE_POSTFIX
+        self.fn_type_node = Employee.FIRSTNAME_PREFIX + Employee.TYPE_POSTFIX
+        self.ln_type_node = Employee.LASTNAME_PREFIX + Employee.TYPE_POSTFIX
 
     @staticmethod
     def create_name(i):
@@ -81,23 +85,20 @@ class Employee:
         edges= []
         if self.id:
             if add_type_nodes:
-                type_node = Employee.ID_PREFIX + Employee.TYPE_POSTFIX
-                edges.append(self.create_edge(to, type_node, self.weight_type_nodes))
-                edges.append(self.create_edge(type_node, self.id, self.weight_id))
+                edges.append(self.create_edge(to, self.id_type_node, self.weight_type_nodes))
+                edges.append(self.create_edge(self.id_type_node, self.id, self.weight_id))
             else:
                 edges.append(self.create_edge(to, self.id, self.weight_id))
         if self.fn:
             if add_type_nodes:
-                type_node = Employee.FIRSTNAME_PREFIX + Employee.TYPE_POSTFIX
-                edges.append(self.create_edge(to, type_node, self.weight_type_nodes))
-                edges.append(self.create_edge(type_node, self.fn, self.weight_fn))
+                edges.append(self.create_edge(to, self.fn_type_node, self.weight_type_nodes))
+                edges.append(self.create_edge(self.fn_type_node, self.fn, self.weight_fn))
             else:
                 edges.append(self.create_edge(to, self.fn, self.weight_fn))
         if self.ln:
             if add_type_nodes:
-                type_node = Employee.LASTNAME_PREFIX + Employee.TYPE_POSTFIX
-                edges.append(self.create_edge(to, type_node, self.weight_type_nodes))
-                edges.append(self.create_edge(type_node, self.ln, self.weight_ln))
+                edges.append(self.create_edge(to, self.ln_type_node, self.weight_type_nodes))
+                edges.append(self.create_edge(self.ln_type_node, self.ln, self.weight_ln))
             else:
                 edges.append(self.create_edge(to, self.ln, self.weight_ln))
         return edges
@@ -108,6 +109,32 @@ class Employee:
         else:
             return (from_node, to_node)
 
+
+class DoubleEmployee(Employee):
+    """Create an two employee's with the same first and/or lastname. One of the employee's may also have an id """
+
+    DOUBLE_NAME_POSTFIX = "x"
+
+    def __init__(self, i, add_id=True, add_type_nodes=False, add_double_fn=True, add_double_ln=True):
+        super().__init__(i, add_id=add_id, add_fn=True, add_ln=True, add_type_nodes=add_type_nodes)
+        self.double_name = self.name + DoubleEmployee.DOUBLE_NAME_POSTFIX
+        self.add_double_fn = add_double_fn
+        self.add_double_ln = add_double_ln
+        self.add_type_nodes = add_type_nodes
+
+    def edges(self):
+        result =  self.connect_to_childs(self.name, self.add_type_nodes)
+        if self.add_double_fn:
+            if self.add_type_nodes:
+                result.append( self.create_edge(self.double_name, self.fn_type_node) )
+            else:
+                result.append( self.create_edge(self.double_name, self.fn) )
+        if self.add_double_ln:
+            if self.add_double_fn:
+                result.append( self.create_edge(self.double_name, self.ln_type_node) )
+            else:
+                result.append( self.create_edge(self.double_name, self.ln) )
+        return result
 
 class DataRoot:
     """Data root test data class."""
@@ -140,6 +167,23 @@ class MatchingCluster:
     def create_name(i):
         return MatchingCluster.NAME_PREFIX + str(i)
 
+
+class Direction:
+    """Direction test data class."""
+    NAME_PREFIX = "DIR"
+    def __init__(self):
+        """Create instance."""
+        self.name = Direction.NAME_PREFIX
+
+class GeneralAgency:
+    """General agency test data class."""
+    NAME_PREFIX = "GA"
+    def __init__(self):
+        """Create instance."""
+        self.name = GeneralAgency.NAME_PREFIX
+
+
+
 # ---------------------------------------------------------------------------------------------------------------------
 # Test data factory
 # ------------------------------------f---------------------------------------------------------------------------------
@@ -169,6 +213,9 @@ class DataFactory:
         """Create new data factory builder instance."""
         pass
 
+    def create_names(self, name, n=10):
+        """Create a list of n names with the name and number."""
+        return [name + str(i) for i in range(0, n)]
 
     def create_graph(self, n=1, connected=False, directed=False, add_dc=False,
                      add_id=True, add_fn=True, add_ln=True, typed=False, weighted=False):
@@ -211,9 +258,36 @@ class DataFactory:
             return graph_builder.create_directed()
         return graph_builder.create()
 
-    def create_names(self, name, n=10):
-        """Create a list of n names with the name and number."""
-        return [name + str(i) for i in range(0, n)]
+    def create_graph_with_doubles(self, n=1, add_dc=False, add_type_nodes=False):
+        """Create n single graphs with MC/DC on one site and EM on the other side.
+
+        If the connected option is enabled, the n single graphs with MC/DC on one site and EM on the other side  will
+        get a root on each side. All MC/DC nodes share a common parent and all EM nodes as well. These two parents
+        will then be connected as well. So all together one single graph will be created!
+        """
+        # init graph builder
+        graph_builder = GraphBuilder()
+        # init direction and general agency
+        dir = Direction()
+        ga = GeneralAgency()
+        # create n subgraphs
+        for i in range(0, n):
+            em = DoubleEmployee(i, add_type_nodes=add_type_nodes)
+            mc = MatchingCluster(i)
+            edges = em.edges_connected_to_childs(mc.name)
+            # add connections to direction and agency
+            edges.append((dir.name, em.name))
+            edges.append((dir.name, mc.name))
+            edges.append((ga.name, em.double_name))
+            # add data cluster (if enabled)
+            if add_dc:
+                dc = DataCollection(i)
+                edges.append((dc.name, mc.name))
+                edges.append((dir.name, dc.name))
+            # add edges
+            graph_builder.append_edges(edges)
+        # create graph and return result
+        return graph_builder.create()
 
 # ---------------------------------------------------------------------------------------------------------------------
 # The end.
